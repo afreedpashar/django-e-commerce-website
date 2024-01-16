146b992182ca8ea5cbd311749c19555c608aec8c
afreed <afreedpasha1998@gmail.com>
2024-01-16 22:41:54 +0530

    new changes in cart

--- ecom/mycart/cart.py
+++ ecom/mycart/cart.py
@@ -13,6 +13,15 @@ class Cart():
         #make sure cart is availableon all pages of site
         self.cart = cart
 
+    def add(self,product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price':str(product.price)}
+        self.session.modified = True
+
+
 
 """
 use thes commands to find the session 
